ea73ea9e0e6bd740e55801981c26f34215e2d1b1
nawinkumar94 <nawinkumar94@gmail.com>
2020-08-06 17:51:10 +0530

    commit to List snapshots,create snapshots for all volumes

--- README.md
+++ README.md
@@ -7,8 +7,19 @@ To list all the instances in the ec2
 ## configure
 Configure our aws using profile
   'aws configure --profile shotty'
+  'pipenv run python ec2-instance-list/ec2-instance-list.py <main command> <sub command> --project=snapshots-myproject'
 
 ## Run
-  'pipenv run python ec2-instance-list/ec2-instance-list.py list --project=snapshots-myproject'
-  'pipenv run python ec2-instance-list/ec2-instance-list.py stop --project=snapshots-myproject'
-  'pipenv run python ec2-instance-list/ec2-instance-list.py start --project=snapshots-myproject'
+  'pipenv run python ec2-instance-list/ec2-instance-list.py'
+  ## To list recent snapshot
+  snapshots list --project=snapshots-myproject
+  ##To list all the snapshots
+   snapshots list --all --project=snapshots-myproject
+  ##To list all the volumes
+    volumes list --project=snapshots-myproject
+  ##To create snapshots for all the volumes
+    volumes create_snapshot --project=snapshots-myproject
+  ##To stop all the intstances
+    instances stop --project=snapshots-myproject
+  ##To start all the intstances
+    instances start --project=snapshots-myproject
--- ec2-instance-list/ec2-instance-list.py
+++ ec2-instance-list/ec2-instance-list.py
@@ -1,5 +1,6 @@
 import boto3
 import click
+import botocore
 
 session=boto3.Session(profile_name='shotty')
 ec2=session.resource('ec2')
@@ -15,8 +16,81 @@ def filter_list(project):
         instances=ec2.instances.all()
     return instances
 
+def has_pending_status(volume):
+    status=list(volume.snapshots.all())
+    return status and status[0].state=='pending'
+
 #'group' in click will group the commands
 @click.group()
+def cli():
+     "Group all the click group commands"
+
+@cli.group()
+def snapshots():
+    "Group all the snapshots for the volumes command"
+
+@snapshots.command('list')
+@click.option("--project", default=None, help="List all the snapshots for volumes based on project")
+@click.option("--all", "list_all" ,default=False ,is_flag=True, help="List all the snapshots for volumes based on project not only the recent one")
+
+def list_snapshots(project,list_all):
+     "List all the Ec2 snapshots for volumes"
+     instances=filter_list(project)
+     for data in instances:
+         for volume in data.volumes.all():
+             for snapshot in volume.snapshots.all():
+                 print(",".join((
+                 data.id,
+                 volume.id,
+                 snapshot.id,
+                 snapshot.state,
+                 snapshot.progress,
+                 snapshot.start_time.strftime("%c"))))
+                 if snapshot.state == 'completed' and not list_all:
+                    break
+     return
+
+@cli.group()
+def volumes():
+    "Group all the volumes command"
+
+@volumes.command('list')
+@click.option("--project",default=None, help="List all the volumes based on project")
+def list_volumes(project):
+    "List all the Ec2 volumes for instances"
+    instances=filter_list(project)
+    for data in instances:
+        for volume in data.volumes.all():
+            print(",".join((
+            data.id,
+            volume.id,
+            volume.state,
+            str(volume.size) + 'GiB',
+            volume.encrypted and 'encrypted' or 'unencrypted')))
+    return
+
+@volumes.command('create_snapshot')
+@click.option("--project",default=None, help=" create snapshots for all the volumes based on project")
+def create_snapshots(project):
+    "Create snapshots for allwait_until_stopped(); the volumes in the instances"
+    instances=filter_list(project)
+    for data in instances:
+        data.stop();
+        print("Waiting untill the instance {0} is stopped".format(data.id))
+        data.wait_until_stopped();
+        for volume in data.volumes.all():
+            if has_pending_status(volume):
+                print("This volume {0} already has pending status snapshot".format(volume.id))
+                continue
+            print("Snapshot is created for the instance {0} voulume {1}".format(data.id,volume.id))
+            volume.create_snapshot(Description="Created by snapshot analyzer")
+            data.start();
+            print("Waiting untill the instance {0} is started".format(data.id))
+            data.wait_until_running();
+    print("All snapshots created!")
+    return
+
+@cli.group()
 def instances():
     "Group all the instances command"
 
@@ -44,8 +118,12 @@ def stop_instances(project):
     "Stop all the Ec2 instances for a specific project"
     instances=filter_list(project)
     for data in instances:
-        print('Stopping the instance {}'.format(data.id))
-        data.stop()
+        print('Stopping the instance of {}'.format(data.id))
+        try:
+            data.stop()
+        except botocore.exceptions.ClientError as exception:
+            print("Could not stop the instance {0} getting ".format(exception) + str(exception))
+            continue
 
     return
 
@@ -55,10 +133,14 @@ def start_instances(project):
     "Start all the Ec2 instances for a specific project"
     instances=filter_list(project)
     for data in instances:
-        print('Start all the instance {}'.format(data.id))
-        data.start()
+        print('Starting the instance of {}'.format(data.id))
+        try:
+            data.start()
+        except botocore.exceptions.ClientError as exception:
+            print("Could not start the instance {0} getting ".format(exception)+str(exception))
+            continue
 
     return
 
 if __name__ == '__main__':
-     instances()
+     cli()
